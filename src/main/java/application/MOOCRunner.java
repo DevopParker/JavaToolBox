2dbb38a03265d8f2e611eb84bda8b874782a43c2
DevopParker <99158883+DevopParker@users.noreply.github.com>
2025-03-28 10:56:04 -0700

    Update MOOCRunner.java Update RESTApi.java Update pom.xml

--- src/main/java/application/MOOCRunner.java
+++ src/main/java/application/MOOCRunner.java
@@ -3,21 +3,19 @@ package application;
 import part1.*;
 
 public class MOOCRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //Printing.applicationTitle();
         //Printing.boilerplate("Example");
         //Printing.fileStructure();
         //Printing.javaCapitalization();
-        Printing.applicationTitle();
-        Menu.runMenu();
-//        try {
-//            RestAPI api = new RestAPI();
-//            String response = api.createUser("John Doe", "leader");
-//
-//            System.out.println("API Response:");
-//            System.out.println(response);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+//        Printing.applicationTitle();
+//        Menu.runMenu();
+
+        RestAPI api = new RestAPI();
+        String response = api.createUser("John Doe");
+
+        System.out.println("API Response:");
+        System.out.println(response);
+
     }
 }
--- src/main/java/application/RestAPI.java
+++ src/main/java/application/RestAPI.java
@@ -1,10 +1,22 @@
 package application;
 
+import java.net.URI;
+import java.net.http.*;
+import org.json.JSONObject;
+
 public class RestAPI {
-    public String createUser() {
+    public String createUser(String name) throws Exception {
+        JSONObject json = new JSONObject();
+        json.put("name", name);
 
-        
+        HttpRequest postRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://reqres.in/api/users"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .build();
 
-        return "none";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(postRequest, HttpResponse.BodyHandlers.ofString());
+        return response.body();
     }
 }

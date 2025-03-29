39f0775ff0f7d2eedd9d523866d9f5697aa3d7e3
DevopParker <99158883+DevopParker@users.noreply.github.com>
2025-03-29 11:18:56 -0700

    Update pom.xml for javafx resources Update MOOCRunner.java for javafx gui

--- src/main/java/application/MOOCRunner.java
+++ src/main/java/application/MOOCRunner.java
@@ -1,20 +1,37 @@
 package application;
 
 import part1.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class MOOCRunner {
+public class MOOCRunner extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("Particle Life Emulator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) throws Exception {
-        //Printing.applicationTitle();
-        //Printing.boilerplate("Example");
-        //Printing.fileStructure();
-        //Printing.javaCapitalization();
-        //Printing.applicationTitle();
-        //Menu.runMenu();
+        launch(args);
 
-        RestAPI api = new RestAPI();
-        //String postResponse = api.createUser("John Doe");
-
-        System.out.println("API Post response:");
-        System.out.println(api.getResponse());
     }
 }
+
+/*
+    Menu.runMenu();
+
+    RestAPI api = new RestAPI();
+    String postResponse = api.createUser("John Doe");
+
+    System.out.println("API Post response:");
+    System.out.println(api.getResponse());
+ */
